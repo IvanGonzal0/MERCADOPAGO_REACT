f1b605895ae23bcb545892f2920385e52331ce4b
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-10-19 13:49:49 -0300

    dotenv

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import Product from './components/product/Product';
+import { initMercadoPago } from "@mercadopago/sdk-react";
 
 
+// REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+initMercadoPago('');
 
 function App() {
   const [count, setCount] = useState(0)
